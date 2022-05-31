7b43bf5c5a5877500c1b0b4d843ed2aab5f9826a
Natali Malich <nv.malich@gmail.com>
2022-05-31 19:27:21 +0100

    Initial Commit

---
+++ LetsGoTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsGoTravel
+//
+//  Created by Natali Malich on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
